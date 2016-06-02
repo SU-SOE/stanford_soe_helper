ccf7f15581d3963e02647e0fb44271f424296f80
Joseph Knox <jgknox@stanford.edu>
2016-06-02 13:42:14 -0700

    added img file, mag glass image, updated styles to call image, updated js file to call the module name

--- js/stanford_news_layouts.js
+++ js/stanford_news_layouts.js
@@ -1,5 +1,5 @@
 (function ($) {
-    Drupal.behaviors.jsinjector1 = {
+    Drupal.behaviors.stanford_soe_helper = {
         attach: function (context, settings) {
             $(".node-type-stanford-news-item .mc-content", context).removeClass("span12").addClass("span10 offset1");
 
