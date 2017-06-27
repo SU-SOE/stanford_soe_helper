480df6d6b5531a5f5b53c326ac07430762a5fca8
Greg Garvey <ggarvey@stanford.edu>
2017-06-27 11:35:36 -0700

    env addition and css changes

--- modules/stanford_soe_helper_magazine/js/stanford_social_widgets.js
+++ modules/stanford_soe_helper_magazine/js/stanford_social_widgets.js
@@ -3,7 +3,14 @@
     attach: function (context, settings) {
       $( window ).load(function() {
         var soeSocialPathToImages = '/sites/default/modules/stanford/stanford_soe_helper/modules/stanford_soe_helper_magazine/img';
-        var soeEnv = '/jse-soe-new-dev';
+        var soePath = location.pathname;
+        var soeEnv = '';
+        if (soePath.indexOf("jse-soe-new-") !== -1) {
+          var parts = soePath.split("/");
+          soeEnv = "/" + parts[0];
+        }
+
+
         $('.group-s-social-and-print.field-group-div').prepend('<div class="widget-wrapper-linkedin"><a href="" class="share-linkedin"><img src="'+ location.protocol + '//' + location.host + soeEnv + soeSocialPathToImages + '/soe_linkedin_icon_blue.png"></a></div>');
         $('.group-s-social-and-print.field-group-div').prepend('<div class="widget-wrapper-twitter"><a href="" class="share-twitter"><img src="'+ location.protocol + '//' + location.host + soeEnv + soeSocialPathToImages + '/soe_twitter_icon_blue.png"></a></div>');
         $('.group-s-social-and-print.field-group-div').prepend('<div class="widget-wrapper-fb"><a href="" class="share-fb"><img src="'+ location.protocol + '//' + location.host + soeEnv + soeSocialPathToImages + '/soe_facebook_icon_blue.png"></a></div>');
