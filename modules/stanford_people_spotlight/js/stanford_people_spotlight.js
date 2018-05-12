16b9b2659aa8d79c87b3e32cc485f68b17fcdc68
Caryl Westerberg <cjwest@acm.org>
2018-05-11 19:16:25 -0700

    SOE-2962: adding spotlight transitions

---
+++ modules/stanford_people_spotlight/js/stanford_people_spotlight.js
@@ -0,0 +1,33 @@
+function spotlightTransition() {
+    var element = document.getElementById("main");
+    element.classList.add("spotlight-transition");
+    element.classList.remove("spotlight");
+}
+
+
+function spotlight() {
+    var element = document.getElementById("main");
+    element.classList.remove("spotlight-transition");
+    element.classList.add("spotlight");
+    document.removeEventListener("click", spotlight);
+
+    // Add the listeners
+    document.getElementById("edit-field-s-ppl-spot-affiliation-tid").addEventListener("click", spotlightTransition);
+    document.getElementById("edit-field-s-ppl-spot-department-tid").addEventListener("click", spotlightTransition);
+    document.getElementById("edit-field-s-ppl-spot-affiliation-tid").addEventListener("blur", spotlight);
+    document.getElementById("edit-field-s-ppl-spot-department-tid").addEventListener("blur", spotlight);
+}
+
+window.onload = function(){
+    spotlight();
+};
+
+
+// Updates the spotlight transition when AJAX completes.
+jQuery(document).ajaxComplete(function(event, xhr, settings) {
+
+    // Is from our view?
+    if (settings.data.indexOf( "view_name=stanford_ppl_spot_3_v_card") != -1) {
+        spotlight();
+    }
+});
