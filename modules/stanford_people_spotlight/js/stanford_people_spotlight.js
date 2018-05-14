a4a22e571a619749531ce4252bf1dcb580bacc56
Caryl Westerberg <cjwest@acm.org>
2018-05-14 16:11:35 -0700

    SOE-2962: updated spotlight transitions

--- modules/stanford_people_spotlight/js/stanford_people_spotlight.js
+++ modules/stanford_people_spotlight/js/stanford_people_spotlight.js
@@ -1,21 +1,23 @@
 function spotlightTransition() {
-    var element = document.getElementById("main");
-    element.classList.add("spotlight-transition");
-    element.classList.remove("spotlight");
+    if (document.getElementsByClassName("view-stanford-ppl-spot-3-v-card").length > 0) {
+        var element = document.getElementById("main");
+        element.classList.add("spotlight-transition");
+        element.classList.remove("spotlight");
+    }
 }
 
 
 function spotlight() {
-    var element = document.getElementById("main");
-    element.classList.remove("spotlight-transition");
-    element.classList.add("spotlight");
-    document.removeEventListener("click", spotlight);
+    if (document.getElementsByClassName("view-stanford-ppl-spot-3-v-card").length > 0) {
+        var element = document.getElementById("main");
+        element.classList.remove("spotlight-transition");
+        element.classList.add("spotlight");
+        document.removeEventListener("click", spotlight);
 
-    // Add the listeners
-    document.getElementById("edit-field-s-ppl-spot-affiliation-tid").addEventListener("click", spotlightTransition);
-    document.getElementById("edit-field-s-ppl-spot-department-tid").addEventListener("click", spotlightTransition);
-    document.getElementById("edit-field-s-ppl-spot-affiliation-tid").addEventListener("blur", spotlight);
-    document.getElementById("edit-field-s-ppl-spot-department-tid").addEventListener("blur", spotlight);
+        // Add the listeners
+        document.getElementById("edit-field-s-ppl-spot-affiliation-tid").addEventListener("blur", spotlight);
+        document.getElementById("edit-field-s-ppl-spot-department-tid").addEventListener("blur", spotlight);
+    }
 }
 
 window.onload = function(){
@@ -30,4 +32,9 @@ jQuery(document).ajaxComplete(function(event, xhr, settings) {
     if (settings.data.indexOf( "view_name=stanford_ppl_spot_3_v_card") != -1) {
         spotlight();
     }
+});
+
+
+jQuery(document).ajaxStart(function() {
+    spotlightTransition();
 });
