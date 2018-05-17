01bb32cd31633183300f4d9a5c80887facf06808
Greg Garvey <ggarvey@stanford.edu>
2018-05-17 14:50:15 -0700

    modified add and js to properly load and function

--- modules/stanford_people_spotlight_views/js/stanford_people_spotlight_views.js
+++ modules/stanford_people_spotlight_views/js/stanford_people_spotlight_views.js
@@ -1,44 +1,48 @@
-function spotlightTransition() {
-    if (document.getElementsByClassName("view-stanford-ppl-spot-3-v-card").length > 0) {
-        var element = document.getElementById("main");
-        element.classList.add("spotlight-transition");
-        if (document.getElementsByClassName("spotlight").length >  0) {
-            element.classList.remove("spotlight");
+(function ($) {
+  Drupal.behaviors.peopleSpotlightViews = {
+    attach: function (context, settings) {
+
+        function spotlightTransition() {
+            if (document.getElementsByClassName("view-stanford-ppl-spot-3-v-card").length > 0) {
+                var element = document.getElementById("main");
+                element.classList.add("spotlight-transition");
+                if (document.getElementsByClassName("spotlight").length >  0) {
+                    element.classList.remove("spotlight");
+                }
+            }
         }
-    }
-}
 
 
-function spotlight() {
-    if (document.getElementsByClassName("view-stanford-ppl-spot-3-v-card").length > 0) {
-        var element = document.getElementById("main");
-        if (document.getElementsByClassName("spotlight-transition").length >  0) {
-            element.classList.remove("spotlight-transition");
+        function spotlight() {
+            if (document.getElementsByClassName("view-stanford-ppl-spot-3-v-card").length > 0) {
+                var element = document.getElementById("main");
+                if (document.getElementsByClassName("spotlight-transition").length >  0) {
+                    element.classList.remove("spotlight-transition");
+                }
+                element.classList.add("spotlight");
+                document.removeEventListener("click", spotlight);
+
+                // Add the listeners
+                document.getElementById("edit-field-s-ppl-spot-affiliation-tid").addEventListener("blur", spotlight);
+                document.getElementById("edit-field-s-ppl-spot-department-tid").addEventListener("blur", spotlight);
+            }
         }
-        element.classList.add("spotlight");
-        document.removeEventListener("click", spotlight);
 
-        // Add the listeners
-        document.getElementById("edit-field-s-ppl-spot-affiliation-tid").addEventListener("blur", spotlight);
-        document.getElementById("edit-field-s-ppl-spot-department-tid").addEventListener("blur", spotlight);
-    }
-}
-
-window.onload = function(){
-    spotlight();
-};
-
-
-// Updates the spotlight transition when AJAX completes.
-jQuery(document).ajaxComplete(function(event, xhr, settings) {
-
-    // Is from our view?
-    if (settings.data.indexOf( "view_name=stanford_ppl_spot_3_v_card") !== -1) {
+        //Let's kick this thing off...
         spotlight();
+
+        // Updates the spotlight transition when AJAX completes.
+        $(document).ajaxComplete(function(event, xhr, settings) {
+            // Is from our view?
+            if (settings.data.indexOf( "view_name=stanford_ppl_spot_3_v_card") !== -1) {
+                spotlight();
+            }
+        });
+
+        $(document).ajaxStart(function() {
+            spotlightTransition();
+        });
+
     }
-});
-
-
-jQuery(document).ajaxStart(function() {
-    spotlightTransition();
-});
+  };
+}(jQuery));
