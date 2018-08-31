378f4f829f45f4facd4e8decf6cfda6a30c33cdf
Greg Garvey <ggarvey@stanford.edu>
2018-08-30 17:09:54 -0700

    refactoring js for universal use

--- modules/stanford_soe_helper_magazine/js/stanford_social_widgets.js
+++ modules/stanford_soe_helper_magazine/js/stanford_social_widgets.js
@@ -2,13 +2,8 @@
   Drupal.behaviors.stanfordSoeHelperMagazine = {
     attach: function (context, settings) {
       $( window ).load(function() {
-        var soeSocialPathToImages = '/sites/default/files';
-        var soePath = location.pathname;
-        var soeEnv = '';
-        if (soePath.indexOf("jse-soe") !== -1) {
-          var parts = soePath.split("/");
-          soeEnv = "/" + parts[1];
-        }
+
+        soeEnv = Drupal.settings.stanford_soe_helper_magazine.servername;
 
         $('.group-s-social-and-print.field-group-div').prepend('<div class="widget-wrapper-linkedin"><a href="" class="share-linkedin"><img src="'+ location.protocol + '//' + location.host + soeEnv + soeSocialPathToImages + '/soe_linkedin_icon_blue.svg" alt="linkedin share"></a></div>');
         $('.group-s-social-and-print.field-group-div').prepend('<div class="widget-wrapper-twitter"><a href="" class="share-twitter"><img src="'+ location.protocol + '//' + location.host + soeEnv + soeSocialPathToImages + '/soe_twitter_icon_blue.svg" alt="twitter share"></a></div>');
